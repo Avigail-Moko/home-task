9f161da1f144b8f50752cfb82b67c222e2de4a8c
Avigail-Moko <avigailmoko@gmail.com>
2024-05-01 16:03:56 +0300

    Finish

--- src/app/ai-car-dealership.service.ts
+++ src/app/ai-car-dealership.service.ts
@@ -16,7 +16,8 @@ export class AiCarDealershipService {
     if (storedArray) {
       this.myArraySubject.next(JSON.parse(storedArray));
     }
-  }
+  } 
+  
 
   public getMyArray(): Observable<any> {
     return this.myArraySubject.asObservable();
